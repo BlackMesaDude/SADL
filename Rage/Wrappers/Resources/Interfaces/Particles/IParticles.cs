52163f2661a28f55206645f0abfffe22cda9d7e6
Francesco Mesiano <francescomesianodev@gmail.com>
2020-07-11 14:17:04 +0200

    Added IParticles.cs

---
+++ Rage/Wrappers/Resources/Interfaces/Particles/IParticles.cs
@@ -0,0 +1,32 @@
+/**
+ *  Copyright (c) 2020 francescomesianodev
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **/
+
+ using TexResources = SADL.Rage.Wrappers.Resources.Interfaces.Textures;
+
+namespace SADL.Rage.Wrappers.Resources.Interfaces.Particles
+{
+    /// <summary>
+    /// Defines a list of particles entities
+    /// </summary>
+    public interface IParticles
+    {
+        /// <summary>
+        /// Gets the dictionary wich stores the texture of the particles
+        /// </summary>
+        TexResources.ITextureDictionary TextureDictionary { get; }
+    }
+}
